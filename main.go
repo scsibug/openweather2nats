cbf6dc6374fecd86a0994a6a4fdb427792e4f7b6
Greg Heartsfield <scsibug@imap.cc>
2020-12-17 13:57:38 -0600

    Simplify generation of JSON data

--- main.go
+++ main.go
@@ -13,42 +13,11 @@ import (
 	"time"
 )
 
-type Location struct {
-	Lon float64 `json:"lon"`
-	Lat float64 `json:"lat"`
-	Zip string  `json:"zip"`
-}
+// Base URL for OpenWeather API ("one-call"), excluding forecasts/alerts
+const openWeatherUrlPrefix = "https://api.openweathermap.org/data/2.5/onecall?exclude=minutely,hourly,daily,alerts&"
 
-type RainVolume struct {
-	OneHour   float64 `json:"rain.1h",omitempty`
-	ThreeHour float64 `json:"rain.3h",omitempty`
-}
-
-type SnowVolume struct {
-	OneHour   float64 `json:"snow.1h",omitempty`
-	ThreeHour float64 `json:"snow.3h",omitempty`
-}
-
-type Weather struct {
-	Date        float64    `json:"dt"`
-	Location    Location   `json:"location"`
-	Description []string   `json:"description",omitempty`
-	Temp        float64    `json:"temp"`
-	FeelsLike   float64    `json:"feels_like",omitempty`
-	DewPoint    float64    `json:"dew_point",omitempty`
-	UVIndex     float64    `json:"uvi",omitempty`
-	Pressure    float64    `json:"pressure",omitempty`
-	Humidity    float64    `json:"humidity",omitempty`
-	Clouds      float64    `json:"clouds",omitempty`
-	WindSpeed   float64    `json:"windspeed",omitempty`
-	WindDegree  float64    `json:"winddeg",omitempty`
-	Visibility  float64    `json:"visibility",omitempty`
-	Sunrise     float64    `json:"sunrise",omitempty`
-	Sunset      float64    `json:"sunset",omitempty`
-	Rain        RainVolume `json:"rain",omitempty`
-	Snow        SnowVolume `json:"snow",omitempty`
-}
-
+// getWeather returns a CloudEvent bytearray for the given openweather url
+// (zip is passed through).
 func getWeather(url string, zipCode string) ([]byte, error) {
 	resp, err := http.Get(url)
 	if err != nil {
@@ -63,43 +32,46 @@ func getWeather(url string, zipCode string) ([]byte, error) {
 	if err := json.Unmarshal(body, &dat); err != nil {
 		panic(err)
 	}
+	// transform openWeather schema to our own Weather struct
 	weather := openWeatherTransform(dat, zipCode)
-	// create JSON
-	wj, err := json.Marshal(weather)
-	if err != nil {
-		panic(err)
-	}
-	ce := openWeatherToCloudEvent(wj)
-	//	fmt.Println(string(body))
-	//	fmt.Println(string(ce))
+	ce := openWeatherToCloudEvent(weather)
 	return ce, nil
 }
 
-func openWeatherTransform(wj map[string]interface{}, zipCode string) Weather {
+func addFloatFromKey(w *map[string]interface{}, api *map[string]interface{}, apikey string, weatherkey string, scale float64) {
+	v, ok := (*api)[apikey].(float64)
+	if ok {
+		(*w)[weatherkey] = v * scale
+	}
+}
+
+// openWeatherTransform converts JSON structure to our own Weather format
+func openWeatherTransform(wj map[string]interface{}, zipCode string) []byte {
 	// Create a JSON structure matching our weather json schema
 	// Create the empty weather struct
-	var w Weather
+	w := make(map[string]interface{})
 	// Get main object
 	main, _ := wj["current"].(map[string]interface{})
-	w.Date = main["dt"].(float64)
-	w.Temp = main["temp"].(float64)
-	w.FeelsLike = main["feels_like"].(float64)
-	w.UVIndex = main["uvi"].(float64)
-	w.Pressure = main["pressure"].(float64)
-	w.Humidity = main["humidity"].(float64) / 100.0
-	w.Sunrise = main["sunrise"].(float64)
-	w.Sunset = main["sunset"].(float64)
-	w.Clouds = main["clouds"].(float64) / 100.0
-	w.WindDegree = main["wind_deg"].(float64)
-	w.WindSpeed = main["wind_speed"].(float64)
-	w.DewPoint = main["dew_point"].(float64)
-	w.Visibility = main["visibility"].(float64)
+	w["dt"] = main["dt"].(float64)
+	// Temperature
+	addFloatFromKey(&w, &main, "temp", "temp", 1.0)
+	addFloatFromKey(&w, &main, "feels_like", "feels_like", 1.0)
+	addFloatFromKey(&w, &main, "uvi", "uvi", 1.0)
+	addFloatFromKey(&w, &main, "pressure", "pressure", 1.0)
+	addFloatFromKey(&w, &main, "humidity", "humidity", (1 / 100.0))
+	addFloatFromKey(&w, &main, "sunrise", "sunrise", 1.0)
+	addFloatFromKey(&w, &main, "sunset", "sunset", 1.0)
+	addFloatFromKey(&w, &main, "clouds", "clouds", (1 / 100.0))
+	addFloatFromKey(&w, &main, "wind_deg", "wind_deg", 1.0)
+	addFloatFromKey(&w, &main, "wind_speed", "wind_speed", 1.0)
+	addFloatFromKey(&w, &main, "dew_point", "dew_point", 1.0)
+	addFloatFromKey(&w, &main, "visibility", "visibility", 1.0)
 	// Get location
-	var loc Location
-	loc.Lat = wj["lat"].(float64)
-	loc.Lon = wj["lon"].(float64)
-	loc.Zip = zipCode
-	w.Location = loc
+	l := make(map[string]interface{})
+	l["lat"] = wj["lat"].(float64)
+	l["lon"] = wj["lon"].(float64)
+	l["zip"] = zipCode
+	w["loc"] = l
 	// Get weather description object
 	desc, _ := main["weather"].([]interface{})
 	descriptions := make([]string, 0)
@@ -107,25 +79,28 @@ func openWeatherTransform(wj map[string]interface{}, zipCode string) Weather {
 		val := v.(map[string]interface{})
 		descriptions = append(descriptions, val["description"].(string))
 	}
-	w.Description = descriptions
+	w["descriptions"] = descriptions
 	// Rain amount
 	rain, ok := main["rain"].(map[string]interface{})
-	var r RainVolume
+	r := make(map[string]interface{})
 	if ok {
-		r.OneHour = rain["1h"].(float64)
-		r.ThreeHour = rain["3h"].(float64)
+		r["1h"] = rain["1h"].(float64)
+		r["3h"] = rain["3h"].(float64)
 	}
-	w.Rain = r
+	w["rain"] = r
 	// Snow amount
 	snow, ok := main["snow"].(map[string]interface{})
-	var s SnowVolume
+	s := make(map[string]interface{})
 	if ok {
-		s.OneHour = snow["1h"].(float64)
-		s.ThreeHour = snow["3h"].(float64)
+		s["1h"] = snow["1h"].(float64)
+		s["3h"] = snow["3h"].(float64)
 	}
-	w.Snow = s
-
-	return w
+	w["snow"] = s
+	wb, err := json.Marshal(w)
+	if err != nil {
+		panic(err)
+	}
+	return wb
 }
 
 func openWeatherToCloudEvent(data []byte) []byte {
@@ -145,7 +120,14 @@ func openWeatherToCloudEvent(data []byte) []byte {
 }
 
 func main() {
-	openWeatherApiUrl := os.Getenv("OPENWEATHER_URL")
+	openWeatherApiKey := os.Getenv("OPENWEATHER_KEY")
+	openWeatherLat := os.Getenv("OPENWEATHER_LAT")
+	openWeatherLon := os.Getenv("OPENWEATHER_LON")
+	openWeatherApiUrl := openWeatherUrlPrefix +
+		"lat=" + openWeatherLat +
+		"&lon=" + openWeatherLon +
+		"&APPID=" + openWeatherApiKey
+
 	// todo err if no api/url defined
 	natsServer := os.Getenv("NATS_SERVER")
 	// todo err if no server defined
